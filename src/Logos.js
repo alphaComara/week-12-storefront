63087398eeba32706213c6acf33d456bce8df17c
Alpha Comara <saliouc@yahoo.com>
2020-08-27 21:18:40 -0400

    Product list in browser

--- src/Logos.js
+++ src/Logos.js
@@ -1,8 +1,34 @@
-import React from 'react'
 
-
-export default () => {
+import React from "react";
+class Logos extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      response: [],
+    };
+  }
+  componentDidMount() {
+    this.callApi()
+      .then((response) => {
+        this.setState({ response });
+      })
+      .catch((err) => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch("http://localhost:3001/logos");
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+  render() {
+    let logos = this.state.response
     return (
-        <div>Logos Place holder</div>
-    )
+      <div>
+          {logos.map((logo) =>
+            <p key={logo._id}>{logo.title}</p>
+          )}
+      </div>
+    );
+  }
 }
+export default Logos;
--- src/Products.js
+++ src/Products.js
@@ -1,35 +1,33 @@
-import React from 'react'
-
+import React from "react";
 class Products extends React.Component {
-    constructor () {
-        super()
-        this.state = {
-            results: []
-        }
-    }
-
-    componentDidMount() {
-        this.callApi()
-        .then(res => this.setState({ response: res.express }))
-        .catch(err => console.log(err));
-    }
-    
-    callApi = async () => {
-        const response = await fetch('http://localhost:3001/products');
-        const body = await response.json();
-        if (response.status !== 200) throw Error(body.message);
-        
-        return body;
+  constructor() {
+    super();
+    this.state = {
+      response: [],
     };
-
-    render ()  {
-        return (
-            <div>
-                <div>Products Place holder</div>
-                <div>{this.state.response}</div>
-            </div>
-        )
-    }
+  }
+  componentDidMount() {
+    this.callApi()
+      .then((response) => {
+        this.setState({ response });
+      })
+      .catch((err) => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch("http://localhost:3001/products");
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+  render() {
+    let products = this.state.response
+    return (
+      <div>
+          {products.map((product) =>
+            <p key={product._id}>{product.title}</p>
+          )}
+      </div>
+    );
+  }
 }
-
-export default Products
+export default Products;
